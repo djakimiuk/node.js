d8c15f9d02ee2fef266e3a02d2d7e042d8f328ed
djakimiuk <djakimiuk@outlook.com>
2023-01-11 14:34:34 +0100

    validation input data

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -2,14 +2,42 @@
 
 // pobierze z API informacje o danej liczbie (http://numbersapi.com/{number}, np http://numbersapi.com/42)
 // informacje pobrane z API zapisze w pliku o pobranej wcześniej nazwie
-
+const fs = require("fs");
 const axios = require("axios");
-async function getNumberInfo(number) {
+const file = "data.json";
+const URL = "http://numbersapi.com/";
+fs.readFile(`${__dirname}/${file}`, "utf-8", (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  if (!data) {
+    console.log("Brak danych do wczytania!");
+    return;
+  }
+  const { number, filename } = JSON.parse(data);
+  if (
+    Number.isInteger(number) ||
+    !number ||
+    !filename ||
+    number == "" ||
+    filename == ""
+  ) {
+    console.log(`Nieprawidłowy format danych wejściowych. Plik data.json powinien wyglądać jak na przykładzie:
+    {
+        "number": "12",
+        "filename": "file.json"
+    }`);
+    return;
+  }
+  getNumberInfo(number, filename);
+});
+
+async function getNumberInfo(number, fileName) {
   try {
-    const response = await axios.get(`http://numbersapi.com/${number}`);
+    const response = await axios.get(`${URL}${number}`);
     console.log(response.data);
   } catch (err) {
-    console.error(err);
+    console.log(err);
   }
 }
-getNumberInfo(21);
